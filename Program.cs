80172b2084c736cef4c591f0419a4ad6929269da
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-22 19:04:34 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Nakov: 01, Branson: 78";
+            string pattern = @"\d+";
+            
+            string replacement = "****";
+            Regex regex = new Regex(pattern);
+
+            string result = regex.Replace(text, replacement);
+
+            Console.WriteLine(result);
+        }
+    }
+}
